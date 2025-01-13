523c17d30f600c835f5172b9722678c57f93a06d
Brishti Saha <brishti@itobuz.com>
2025-01-13 05:27:55 -0800

    wip

---
+++ main.js
@@ -0,0 +1,7 @@
+const number = document.querySelectorAll(".num");
+const inputBox = document.getElementById("calculator-input");
+
+
+number.forEach((item) => {
+  item.addEventListener("click", number.innerHTML);
+});
