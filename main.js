cc94e6def91b8caaebf8ffb587d97cc079a38e90
Brishti Saha <brishti@itobuz.com>
2025-01-14 05:24:22 -0800

    work in progress

--- main.js
+++ main.js
@@ -1,7 +1,43 @@
-const number = document.querySelectorAll(".num");
-const inputBox = document.getElementById("calculator-input");
+const num = document.querySelectorAll(".num");
+const display = document.getElementById("calculator-input");
+const operators = document.querySelectorAll(".operator");
+const equals = document.querySelector(".equals");
 
+let currentNum = "";
+let nextNum = "";
+let operator = "";
 
-number.forEach((item) => {
-  item.addEventListener("click", number.innerHTML);
+function calculation() {
+  if (operator !== "") {
+    nextNum = display.value.innerHTML;
+    console.log(nextNum);
+  } else {
+    currentNum = display.value.innerHTML;
+    console.log(currentNum);
+  }
+}
+
+equals.addEventListener("click", () => {
+  let output = 0;
+  currentNum = currentNum.toString().slice(1);
+  console.log(currentNum);
+});
+
+num.forEach((item) => {
+  item.addEventListener("click", () => {
+    if (operators === "") {
+      display.value += item.innerHTML;
+      currentNum = display.value;
+    } else {
+      display.value += item.innerHTML;
+      nextNum = display.value;
+    }
+  });
+});
+
+operators.forEach((item) => {
+  item.addEventListener("click", () => {
+    display.value = item.innerHTML;
+    operators = display.value;
+  });
 });
