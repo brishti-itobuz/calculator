0f2ff74de77f8020aa6da6bab030e15e9e5c4f30
Brishti Saha <brishti@itobuz.com>
2025-01-14 05:26:01 -0800

    work in progress

--- main.js
+++ main.js
@@ -7,20 +7,9 @@ let currentNum = "";
 let nextNum = "";
 let operator = "";
 
-function calculation() {
-  if (operator !== "") {
-    nextNum = display.value.innerHTML;
-    console.log(nextNum);
-  } else {
-    currentNum = display.value.innerHTML;
-    console.log(currentNum);
-  }
-}
-
 equals.addEventListener("click", () => {
   let output = 0;
   currentNum = currentNum.toString().slice(1);
-  console.log(currentNum);
 });
 
 num.forEach((item) => {
